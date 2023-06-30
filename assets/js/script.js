0bfddda5f4e11b52f4d5e47a748fedddd497be03
4nndmw <anandamaulanaw1850@gmail.com>
2023-06-30 19:36:00 +0700

    update responsive

--- assets/js/script.js
+++ assets/js/script.js
@@ -41,23 +41,20 @@ var slideIndex = 1;
 showSlides(slideIndex);
 
 function plusSlides(n) {
-  showSlides(slideIndex += n);
+  showSlides((slideIndex += n));
 }
 
 function showSlides(n) {
   var i;
   var slides = document.getElementsByClassName("slide");
-  if (n > slides.length)
-  {
+  if (n > slides.length) {
     slideIndex = 1;
   }
-  if (n < 1)
-  {
-    slideIndex = slides.length
+  if (n < 1) {
+    slideIndex = slides.length;
   }
-  for (i = 0; i < slides.length; i++)
-  {
+  for (i = 0; i < slides.length; i++) {
     slides[i].style.display = "none";
   }
-  slides[slideIndex-1].style.display = "block";
+  slides[slideIndex - 1].style.display = "block";
 }
