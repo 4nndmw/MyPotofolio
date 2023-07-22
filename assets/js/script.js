f2411401b032879e890665f866c516800c616db7
4nndmw <anandamaulanaw1850@gmail.com>
2023-07-23 00:01:29 +0700

    update responsive

--- assets/js/script.js
+++ assets/js/script.js
@@ -41,111 +41,106 @@ $(".skill-per").each(function () {
 
 var loader = document.getElementById("preloader");
 
-window.addEventListener("load", function(){
+window.addEventListener("load", function () {
   loader.style.display = "none";
-})
+});
 var loader = document.getElementById("preloader");
 
-window.addEventListener("load", function(){
-  setTimeout(function() {
+window.addEventListener("load", function () {
+  setTimeout(function () {
     loader.style.display = "none";
   }, 2000); // 2000 milliseconds = 2 seconds
 });
 
-
-
-/**
- * Creator           : Coding's Time
- * Youtube Channel   : https://www.youtube.com/channel/UC6dnKqrImGWqMb9ty1n0Ziw
- * Github Profile    : https://github.com/codingstime
- */
-
 let onSlide = false;
 
 window.addEventListener("load", () => {
-   autoSlide();
+  autoSlide();
 
-   const dots = document.querySelectorAll(".carousel_dot");
-   for (let i = 0; i < dots.length; i++) {
-      dots[i].addEventListener("click", () => slide(i));
-   }
+  const dots = document.querySelectorAll(".carousel_dot");
+  for (let i = 0; i < dots.length; i++) {
+    dots[i].addEventListener("click", () => slide(i));
+  }
 
-   const buttonPrev = document.querySelector(".carousel_button__prev");
-   const buttonNext = document.querySelector(".carousel_button__next");
-   buttonPrev.addEventListener("click", () => slide(getItemActiveIndex() - 1));
-   buttonNext.addEventListener("click", () => slide(getItemActiveIndex() + 1));
-})
+  const buttonPrev = document.querySelector(".carousel_button__prev");
+  const buttonNext = document.querySelector(".carousel_button__next");
+  buttonPrev.addEventListener("click", () => slide(getItemActiveIndex() - 1));
+  buttonNext.addEventListener("click", () => slide(getItemActiveIndex() + 1));
+});
 
 function autoSlide() {
-   setInterval(() => {
-      slide(getItemActiveIndex() + 1);
-   }, 3000); // slide speed = 3s
+  setInterval(() => {
+    slide(getItemActiveIndex() + 1);
+  }, 3000); // slide speed = 3s
 }
 
 function slide(toIndex) {
-   if (onSlide)
-      return;
-   onSlide = true;
+  if (onSlide) return;
+  onSlide = true;
 
-   const itemsArray = Array.from(document.querySelectorAll(".carousel_item"));
-   const itemActive = document.querySelector(".carousel_item__active");
-   const itemActiveIndex = itemsArray.indexOf(itemActive);
-   let newItemActive = null;
+  const itemsArray = Array.from(document.querySelectorAll(".carousel_item"));
+  const itemActive = document.querySelector(".carousel_item__active");
+  const itemActiveIndex = itemsArray.indexOf(itemActive);
+  let newItemActive = null;
 
-   if (toIndex > itemActiveIndex) {
-      // check if toIndex exceeds the number of carousel items
-      if (toIndex >= itemsArray.length) {
-         toIndex = 0;
-      }
+  if (toIndex > itemActiveIndex) {
+    // check if toIndex exceeds the number of carousel items
+    if (toIndex >= itemsArray.length) {
+      toIndex = 0;
+    }
 
-      newItemActive = itemsArray[toIndex];
+    newItemActive = itemsArray[toIndex];
 
-      // start transition
-      newItemActive.classList.add("carousel_item__pos_next");
-      setTimeout(() => {
-         newItemActive.classList.add("carousel_item__next");
-         itemActive.classList.add("carousel_item__next");
-      }, 20);
-   } else {
-      // check if toIndex exceeds the number of carousel items
-      if (toIndex < 0) {
-         toIndex = itemsArray.length - 1;
-      }
+    // start transition
+    newItemActive.classList.add("carousel_item__pos_next");
+    setTimeout(() => {
+      newItemActive.classList.add("carousel_item__next");
+      itemActive.classList.add("carousel_item__next");
+    }, 20);
+  } else {
+    // check if toIndex exceeds the number of carousel items
+    if (toIndex < 0) {
+      toIndex = itemsArray.length - 1;
+    }
 
-      newItemActive = itemsArray[toIndex];
+    newItemActive = itemsArray[toIndex];
 
-      // start transition
-      newItemActive.classList.add("carousel_item__pos_prev");
-      setTimeout(() => {
-         newItemActive.classList.add("carousel_item__prev");
-         itemActive.classList.add("carousel_item__prev");
-      }, 20);
-   }
+    // start transition
+    newItemActive.classList.add("carousel_item__pos_prev");
+    setTimeout(() => {
+      newItemActive.classList.add("carousel_item__prev");
+      itemActive.classList.add("carousel_item__prev");
+    }, 20);
+  }
 
-   // remove all transition class and switch active class
-   newItemActive.addEventListener("transitionend", () => {
+  // remove all transition class and switch active class
+  newItemActive.addEventListener(
+    "transitionend",
+    () => {
       itemActive.className = "carousel_item";
       newItemActive.className = "carousel_item carousel_item__active";
       onSlide = false;
-   }, {
-      once: true
-   });
+    },
+    {
+      once: true,
+    }
+  );
 
-   slideIndicator(toIndex);
+  slideIndicator(toIndex);
 }
 
 function getItemActiveIndex() {
-   const itemsArray = Array.from(document.querySelectorAll(".carousel_item"));
-   const itemActive = document.querySelector(".carousel_item__active");
-   const itemActiveIndex = itemsArray.indexOf(itemActive);
-   return itemActiveIndex;
+  const itemsArray = Array.from(document.querySelectorAll(".carousel_item"));
+  const itemActive = document.querySelector(".carousel_item__active");
+  const itemActiveIndex = itemsArray.indexOf(itemActive);
+  return itemActiveIndex;
 }
 
 function slideIndicator(toIndex) {
-   const dots = document.querySelectorAll(".carousel_dot");
-   const dotActive = document.querySelector(".carousel_dot__active");
-   const newDotActive = dots[toIndex];
+  const dots = document.querySelectorAll(".carousel_dot");
+  const dotActive = document.querySelector(".carousel_dot__active");
+  const newDotActive = dots[toIndex];
 
-   dotActive.classList.remove("carousel_dot__active");
-   newDotActive.classList.add("carousel_dot__active");
+  dotActive.classList.remove("carousel_dot__active");
+  newDotActive.classList.add("carousel_dot__active");
 }
