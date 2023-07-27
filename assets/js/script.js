b1dd0719914594a4b24c8d1f915c2b1e65db95a3
4nndmw <anandamaulanaw1850@gmail.com>
2023-07-28 01:01:37 +0700

    new update

--- assets/js/script.js
+++ assets/js/script.js
@@ -153,13 +153,3 @@ function slideIndicator(toIndex) {
 
 
 
-
-// skills
-
-jQuery(document).ready(function(){
-	jQuery('.skillbar').each(function(){
-		jQuery(this).find('.skillbar-bar').animate({
-			width:jQuery(this).attr('data-percent')
-		},6000);
-	});
-});
